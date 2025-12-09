b645078d84e0b821d86ff5ff9b0d9d3fcbf9a8ad
ujin22 <p.b.m.aubert@gmail.com>
2025-12-09 15:17:25 +0100

    Install table.js, add commands for sum of all expenses, list all and delete expense

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import Table from "cli-table3";
 
 import { writeFile, readFile } from "fs/promises";
 
@@ -18,14 +19,20 @@ const initializeEntity = async (options) => {
 		process.stderr.write("error while writing file");
 		return [];
 	}
-}
+};
 
 const saveExpense = async (options) => {
+
+	if (Number.isNaN(Number(options.amount))) {
+		console.error("enter a valid numeric amount!")
+		return;
+	}
+
 	const expensesFromFile = await loadExpenses();
 
 	if (!expensesFromFile) {
 		const initializedEntity = await initializeEntity(options);
-		await writeFile("./expenses.json", JSON.stringify(initializedEntity));
+		await writeFile("./expenses.json", JSON.stringify(initializedEntity, null, 2));
 	}
 
 	const newExpense = {
@@ -39,7 +46,7 @@ const saveExpense = async (options) => {
 		await writeFile("./expenses.json", JSON.stringify(expensesFromFile, null, 2));
 		process.stdout.write("Expense added!")
 	}
-}
+};
 
 const loadExpenses = async () => {
 	try {
@@ -48,19 +55,83 @@ const loadExpenses = async () => {
 			const content = JSON.parse(fileContentRaw);
 			return content;
 		} catch(e) {
-			if (!content || typeof content !== 'File structure damaged') {
+			if (!content || typeof content !== "File structure damaged \n") {
 				process.stderr.write(e);
 				return [];
 			}
 		}
 	} catch (e) {
 		if (e.code === 'ENOENT') {
-			process.stdout.write("File doesn't exist");
+			process.stdout.write("File 'Expenses.json' doesn't exist \n");
 			return []; 
 		} 
-		process.stderr.write("Unexpected error trying to import file!")
+		process.stderr.write("Unexpected error trying to import file! \n")
 	}
+};
+
+
+const deleteExepnse = async (options) => {
+	const allExpenses = await loadExpenses();
+	const idToDelete = Number(options.id);
+	const filteredExpenses = allExpenses.filter(expense => expense.id !== idToDelete);
+	await writeFile("./expenses.json", JSON.stringify(filteredExpenses, null, 2));
+	process.stdout.write("Expense deleted successfully \n");
 }
+
+
+const listAll = async () => {
+	const allExpenses = await loadExpenses();
+  
+	const table = new Table({
+		head: ["ID", "Date", "Description", "Amount"],
+	});
+  
+	allExpenses.forEach(e => {
+		table.push([
+			e.id,
+			e.time.slice(0,10),
+			e.description,
+			e.amount
+		]);
+	});
+  	console.log(table.toString());
+};
+
+const sumExpenses = async () => {
+	const allExpenses = await loadExpenses();
+	if (!allExpenses.length) {
+		process.stdout.write("No expenses traced! \n")
+	} else {
+		return allExpenses.reduce((a,b) => {
+			a.amount + b.amount;
+		});
+	}
+};
+
+//TODO: 
+
+/**
+ * 
+ * COMMANDS: 
+ * 
+ * ADD MODIFIYING AMOUNT
+ * 
+ * ADD SORTING BY MONTH
+ * 
+ * 
+ * expense-tracker/
+ * 
+ * SOME REFACTORING IDEAS: 
+ * 
+ * 
+ * 
+ * 
+├── cli.js                # commander setup
+└── lib/
+    ├── store.js          # load/save JSON file
+    ├── expenses.js       # business logic (add/list/remove)
+    └── utils.js          # small helpers (id generation, validation)
+*/
 
 program
 	.command('add')
@@ -68,6 +139,24 @@ program
 	.option('--description <expense>')
 	.option('--amount, <amount>')
 	.action((options)=> saveExpense(options))
+
+program
+	.command('delete')
+	.description('delete one expense')
+	.option('--id, <id>')
+	.action((options) => deleteExepnse(options));
+
+program
+	.command('list')
+	.description('list all expenses')
+	.action(()=>listAll());
+
+program
+	.command('sum')
+	.description('sum all expenses')
+	.action(()=>sumExpenses());
+
+
 
 program.parse();
 
